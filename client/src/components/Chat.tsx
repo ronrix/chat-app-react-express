9e407214d4ea394420c657ae327815fa82eb18a3
ronrix <ronrixl5@gmail.com>
2023-07-04 21:56:20 +0800

    fix: open chat and delete button propagation by moving delete button outside of LInk component

--- client/src/components/Chat.tsx
+++ client/src/components/Chat.tsx
@@ -7,7 +7,7 @@ import {
   ListItemSuffix,
   IconButton,
 } from "@material-tailwind/react";
-import { useEffect, useContext, useState } from "react";
+import { useEffect, useContext } from "react";
 import { Link } from "react-router-dom";
 import { MessageContext, MessageContextType } from "../context/message.context";
 import { socket } from "../pages/Dashboard";
@@ -41,8 +41,6 @@ export default function Chats(props: Props) {
     messageContext?.setChatUser({ id, username, isOnline, roomId });
   };
 
-  // this function shows the
-
   useEffect(() => {
     return () => {
       socket.off("join_room"); // remove the listener 'join_room' on unmount
@@ -51,14 +49,18 @@ export default function Chats(props: Props) {
 
   return (
     <ListItem>
-      <ListItemPrefix>
-        <Avatar
-          variant='circular'
-          alt='alexander'
-          src='https://images.unsplash.com/photo-1578632767115-351597cf2477?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80'
-        />
-      </ListItemPrefix>
-      <Link to={`/dashboard/inbox/${roomId}}`} onClick={handleSettingActiveMsg}>
+      <Link
+        to={`/dashboard/inbox/${roomId}}`}
+        onClick={handleSettingActiveMsg}
+        className='flex-1 flex m-0 p-0'
+      >
+        <ListItemPrefix>
+          <Avatar
+            variant='circular'
+            alt='alexander'
+            src='https://images.unsplash.com/photo-1578632767115-351597cf2477?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80'
+          />
+        </ListItemPrefix>
         <div>
           <Typography
             variant='h6'
